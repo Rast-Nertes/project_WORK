b04a41455a24569c6a973071b95865f1fa12e876
Eldar <eldarkad122@gmail.com>
2024-11-12 18:27:50 +0300

    Пункт 4_2

--- tc2024/routes/index.js
+++ tc2024/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/manual', function(req, res, next) {
+  res.send('<h1>Manyal page</h1>');
+});
+
+router.get('/mental', function(req, res, next) {
+  res.send('<h1>Mental page</h1>');
+});
+
+router.get('/sedent', function(req, res, next) {
+  res.send('<h1>Sedent page</h1>');
 });
 
 module.exports = router;
