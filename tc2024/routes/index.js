c0c53457398d6c4b173384bb494a6c242191f04f
Eldar <eldarkad122@gmail.com>
2024-11-12 21:37:20 +0300

    tc_4_3

--- tc2024/routes/index.js
+++ tc2024/routes/index.js
@@ -2,16 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/manual', function(req, res, next) {
-  res.send('<h1>Manyal page</h1>');
+router.get('/mental', function(req, res, next) {
+  res.render('work', {
+    title:"Mental",
+    picture:"/images/mental.png",
+    desc:"Говорят, что от долгих раздумий человек устает еще сильнее, чем от физических нагрузок. Так ли это?"
+  });
 });
 
-router.get('/mental', function(req, res, next) {
-  res.send('<h1>Mental page</h1>');
+router.get('/manual', function(req, res, next) {
+  res.render('work', {
+    title:"Manual",
+    picture:"/images/manual.png",
+    desc:"Ручная работа бывает самой тяжелой."
+  });
 });
 
 router.get('/sedent', function(req, res, next) {
-  res.send('<h1>Sedent page</h1>');
+  res.render('work', {
+    title:"Sedent",
+    picture:"/images/Sedentary.png",
+    desc:"Сидячая работа может плохо сказаться на вашем позвоночнике, не забывайте это."
+  });
 });
 
 module.exports = router;
