dc9b42720e2aac1972adb7e1c3bcd7d591bf3014
Eldar <eldarkad122@gmail.com>
2024-11-13 19:08:05 +0300

    tc_6_2

---
+++ tc2024/data.js
@@ -0,0 +1,20 @@
+var data = [{
+    title: 'Manual',
+    nick: 'manual',
+    avatar: '/images/manual.jpeg',
+    desc: 'Ручная работа одна из самых тяжелых.'
+},
+{
+    title: 'Mental',
+    nick: 'mental',
+    avatar: '/images/manual.png',
+    desc: 'От умственной работы человек тоже устает.'
+},
+{
+    title: 'Sedent',
+    nick: 'sedent',
+    avatar: '/images/Sedentary.png',
+    desc: 'Сидячая работа может скзааться на вашем позвоночнике, имейте в виду.'
+}
+];
+module.exports.data = data;
