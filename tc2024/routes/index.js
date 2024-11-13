224268461fb4d45cdf18fc74b0d367235095470b
Eldar <eldarkad122@gmail.com>
2024-11-13 15:31:49 +0300

    tc_5_1

--- tc2024/routes/index.js
+++ tc2024/routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 router.get('/mental', function(req, res, next) {
   res.render('work', {
     title:"Mental",
