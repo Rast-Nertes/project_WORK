239fa85b8be75fccfd6569235f4d1d29f5e42b50
Eldar <eldarkad122@gmail.com>
2024-11-14 19:27:20 +0300

    tc_7_3

--- tc2024/testMongoose.js
+++ tc2024/testMongoose.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-
-var schema = mongoose.Schema({ name: String })
-schema.methods.work = function(){
-    console.log(this.name + " начало работу")
-}
-const work = mongoose.model('work', schema);
-const hard_work = new work({name: 'sedent_work'});
-hard_work.save().then(() =>hard_work.work());
+var Work = require('./models/work.js').work
+var work = new Work({
+    title: "Сидячая работа",
+    nick:'sedent'
+})
+work.save();
