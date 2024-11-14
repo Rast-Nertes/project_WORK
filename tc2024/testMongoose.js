ff4225a4c7096d5df87cd13659a34e0c4d6b8a80
Eldar <eldarkad122@gmail.com>
2024-11-14 18:36:10 +0300

    tc_7_2

--- tc2024/testMongoose.js
+++ tc2024/testMongoose.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-const work = mongoose.model('work', { name: String });
-const hard_work = new work({ name: 'sedent' });
-hard_work.save().then(() => console.log('Start work!'));
+
+var schema = mongoose.Schema({ name: String })
+schema.methods.work = function(){
+    console.log(this.name + " начало работу")
+}
+const work = mongoose.model('work', schema);
+const hard_work = new work({name: 'sedent_work'});
+hard_work.save().then(() =>hard_work.work());
