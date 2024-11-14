1006f08b8bef18aa5d5749e83794087e3ec89837
Eldar <eldarkad122@gmail.com>
2024-11-14 20:04:08 +0300

    tc_8_2

--- tc2024/app.js
+++ tc2024/app.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb://localhost/tc2024')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var works = require('./routes/works');
 
 // Сидячая, ручная, умственная работа | Work sedentary, manual, mental
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/works', works);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
