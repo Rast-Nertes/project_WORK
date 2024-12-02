95e65f80de1ae0930c29c0fdc16d5d0091762a01
Eldar <eldarkad122@gmail.com>
2024-12-02 19:47:02 +0300

    tc_9_5

--- tc2024/app.js
+++ tc2024/app.js
@@ -45,6 +45,7 @@ app.use(function(req,res,next){
   next()
 })
 
+app.use(require("./middlewares/createMenu.js"))
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/works', works);
---
+++ tc2024/middlewares/createMenu.js
@@ -0,0 +1,12 @@
+var Work = require("../models/work").work
+    module.exports = async function(req,res,next){
+    res.locals.nav = []
+    
+    var menu = await Work.find(null,{_id:0,title:1,nick:1});
+    console.log(menu);
+    
+    if (menu.length != 0) {
+        res.locals.nav = menu;
+    }
+    next();
+}
