c0a167535bfde44cc04f9a698d0d141f3fa574fb
Eldar <eldarkad122@gmail.com>
2024-12-02 20:53:31 +0300

    tc_10_1

--- tc2024/routes/index.js
+++ tc2024/routes/index.js
@@ -2,31 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', counter:req.session.counter });
+router.get('/', function (req, res, next) {
+  req.session.greeting = "Hi!!!";
+  res.render('index', { title: 'Express', counter: req.session.counter });
 });
 
-router.get('/mental', function(req, res, next) {
+/* GET login/registration page. */
+router.get('/logreg', function (req, res, next) {
+  res.render('logreg', { title: 'Вход' });
+});
+
+router.get('/mental', function (req, res, next) {
   res.render('work', {
-    title:"Mental",
-    picture:"/images/mental.png",
-    desc:"Говорят, что от долгих раздумий человек устает еще сильнее, чем от физических нагрузок. Так ли это?"
+    title: "Mental",
+    picture: "/images/mental.png",
+    desc: "Говорят, что от долгих раздумий человек устает еще сильнее, чем от физических нагрузок. Так ли это?"
   });
 });
 
-router.get('/manual', function(req, res, next) {
+router.get('/manual', function (req, res, next) {
   res.render('work', {
-    title:"Manual",
-    picture:"/images/manual.png",
-    desc:"Ручная работа бывает самой тяжелой."
+    title: "Manual",
+    picture: "/images/manual.png",
+    desc: "Ручная работа бывает самой тяжелой."
   });
 });
 
-router.get('/sedent', function(req, res, next) {
+router.get('/sedent', function (req, res, next) {
   res.render('work', {
-    title:"Sedent",
-    picture:"/images/Sedentary.png",
-    desc:"Сидячая работа может плохо сказаться на вашем позвоночнике, не забывайте это."
+    title: "Sedent",
+    picture: "/images/Sedentary.png",
+    desc: "Сидячая работа может плохо сказаться на вашем позвоночнике, не забывайте это."
   });
 });
 
