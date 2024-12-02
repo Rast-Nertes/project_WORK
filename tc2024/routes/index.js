eabfe08afd3b13457ba557a352a4846f3eefe14c
Eldar <eldarkad122@gmail.com>
2024-12-02 21:52:47 +0300

    tc_10_6

--- tc2024/routes/index.js
+++ tc2024/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user').User;
 
+/* GET home page. */
 router.get('/', function (req, res, next) {
   req.session.greeting = "Hi!!!";
   res.render('index', { title: 'Express', counter: req.session.counter });
@@ -9,21 +10,18 @@ router.get('/', function (req, res, next) {
 
 /* GET login/registration page. */
 router.get('/logreg', function (req, res, next) {
-  res.render('logreg', { title: 'Вход' });
+  res.render('logreg', { title: 'Вход', error: null });
 });
+
 
 /* POST login/registration page. */
 router.post('/logreg', async function (req, res, next) {
   var username = req.body.username
   var password = req.body.password
-  console.log(username);
-  console.log(password);
   var users = await User.find({ username: username });
-  console.log(users);
   if (!users.length) {
     //res.send("<h1>Пользователь НЕ найден</h1>");
-    var user = new
-      User({ username: username, password: password })
+    var user = new User({ username: username, password: password })
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/');
@@ -34,33 +32,10 @@ router.post('/logreg', async function (req, res, next) {
       req.session.user_id = foundUser._id
       res.redirect('/')
     } else {
-      res.render('logreg', { title: 'Вход' });
+      res.render('logreg', { title: 'Вход', error: 'Пароль не верный' });
     }
   }
 });
 
-router.get('/mental', function (req, res, next) {
-  res.render('work', {
-    title: "Mental",
-    picture: "/images/mental.png",
-    desc: "Говорят, что от долгих раздумий человек устает еще сильнее, чем от физических нагрузок. Так ли это?"
-  });
-});
-
-router.get('/manual', function (req, res, next) {
-  res.render('work', {
-    title: "Manual",
-    picture: "/images/manual.png",
-    desc: "Ручная работа бывает самой тяжелой."
-  });
-});
-
-router.get('/sedent', function (req, res, next) {
-  res.render('work', {
-    title: "Sedent",
-    picture: "/images/Sedentary.png",
-    desc: "Сидячая работа может плохо сказаться на вашем позвоночнике, не забывайте это."
-  });
-});
 
 module.exports = router;
